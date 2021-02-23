6ba846d94684b5aff0ed17b328e6c4e1ec7bcd97
Sofia7311 <79221522+Sofia7311@users.noreply.github.com>
2021-02-23 00:09:03 +0000

    Update app.py
    updated variable names

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ def home():
     return render_template('home.html')
 @app.route('/', methods=['POST'])
 def getvalue():
-    user_guess_number = request.form['guessNumber']
-    return render_template('pass.html', num=user_guess_number)
+    user_input_number = request.form['inputNumber']
+    return render_template('pass.html', num=user_input_number)
 
 if __name__ == '__main__':
     app.run()
